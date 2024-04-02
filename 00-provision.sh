4a49191c45a4d2d46951884c650e1884f6464dc9
Ton Machielsen <ton@machielsen.net>
2024-04-02 17:04:26 +0200

    Added pgbench initialization

--- 00-provision.sh
+++ 00-provision.sh
@@ -7,16 +7,13 @@ tpaexec configure pemdemo -a M1 --platform docker --postgresql 15 --enable-patro
 cp configyml.backup pemdemo/config.yml
 tpaexec provision pemdemo && tpaexec deploy pemdemo
 
-docker exec pg1 sh -c 'echo "shared_preload_libraries = '\''\$libdir/dbms_pipe, \$libdir/edb_gen, \$libdir/dbms_aq, edb_wait_states, sql-profiler, index_advisor'\''" >> /opt/postgres/data/conf.d/9999-override.conf'
-docker exec pg2 sh -c 'echo "shared_preload_libraries = '\''\$libdir/dbms_pipe, \$libdir/edb_gen, \$libdir/dbms_aq, edb_wait_states, sql-profiler, index_advisor'\''" >> /opt/postgres/data/conf.d/9999-override.conf'
-docker exec -it pg1 bash -c "sudo su - enterprisedb -c 'pg_ctl restart'"
-docker exec -it pg2 bash -c "sudo su - enterprisedb -c 'pg_ctl restart'"
-
 EDBPASSWORD=$(tpaexec show-password pemdemo enterprisedb)
-DBAPASSWORD=$(tpaexec show-password pemdemo enterprisedb)
+DBAPASSWORD=$(tpaexec show-password pemdemo dba)
 
 #IP=$(hostname -i)
 IP=localhost
+printf "${G}--- Initializing pgbench on ${R}pg1${G} --- ${N}\n"
+PGPASSWORD=$EDBPASSWORD pgbench -h $IP -p 5444 -i -U enterprisedb postgres
 printf "${G}--- Provisioning complete. You can now access PEM on ${R}https://$IP/pem${G} using userid ${R}enterprisedb${G} and password ${R}$EDBPASSWORD\n"
 printf "${G}--- There is also a user ${R}dba${G} with password ${R}$DBAPASSWORD\n"
 
